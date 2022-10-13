58f459a2c5f4b302a3f13d6bfd4c7070371d6c82
NamPhunPhin <Shagara258@gmail.com>
2022-10-13 11:14:34 +0700

    upload

--- wp-login.php
+++ wp-login.php
@@ -44,7 +44,7 @@
 		</form>
 
 					<p id="nav">
-								<a href="https://namphunphin.github.io/mywordpress/lost-password/">Bạn quên mật khẩu?</a>
+								<a href="https://namphunphin.github.io/mywordpress/tai-khoan/lost-password/">Bạn quên mật khẩu?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
@@ -102,7 +102,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://namphunphin.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=6.0.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"d8ab6f02ba"};
+var userProfileL10n = {"user_id":"0","nonce":"69b0b5c8c8"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
